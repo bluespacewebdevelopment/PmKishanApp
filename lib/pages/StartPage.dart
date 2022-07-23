cde0d1fdf37a9b2fd4f50aa924cce08082517e11
BhagchandJat12 <6686@vitj.ac.in>
2022-07-24 01:04:52 +0530

    three

---
+++ lib/pages/StartPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {  },
+          style: ButtonStyle(
+
+          ),
+          child: Text("START"),),
+      ),
+    );
+  }
+}
