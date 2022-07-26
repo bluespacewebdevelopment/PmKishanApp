7f812b75f19d6b5158e75aeb1aaa9393cbd85696
BhagchandJat12 <6686@vitj.ac.in>
2022-07-26 20:28:17 +0530

    2

--- lib/pages/web_view.dart
+++ lib/pages/web_view.dart
@@ -1,5 +1,8 @@
+
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 
 class Web_View extends StatefulWidget {
   Web_View({Key? key, required this.url}) : super(key: key);
@@ -10,43 +13,64 @@ class Web_View extends StatefulWidget {
 
 class _Web_ViewState extends State<Web_View> {
   String url;
+
+  bool flag=false;
   late WebViewController controller;
   double webprogress=0;
   _Web_ViewState(this.url);
+  show() async{
+        flag=await InternetConnectionChecker().hasConnection ;
+        setState(() {
+
+        });
+  }
+  @override
+  void didUpdateWidget(covariant Web_View oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    show();
+    print(flag);
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      show();
+    });
 
-    return Scaffold(
+    return flag?
+      Scaffold(
       appBar: AppBar(
         title: Text('किसान सम्मान निधि'),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         elevation: 0.2,
       ),
-      body: Column(
-          children: [
-            webprogress<1?
-            SizedBox(
-              child: LinearProgressIndicator(
-                value: webprogress,
-                color: Colors.red,
-                backgroundColor: Colors.black,
-              ),
-            ):SizedBox(),
-            Expanded(
-              child: WebView(
-                initialUrl: url,
-                javascriptMode: JavascriptMode.unrestricted,
-                onProgress:(progress){
-                  setState(() {
-                    this.webprogress=progress/100;
-                  });
-                },
-              ),
-            ),
-          ],
-        ),
-   
+      body:
+    Column(
+    children: [
+    webprogress<1?
+    SizedBox(
+    child: LinearProgressIndicator(
+    value: webprogress,
+    color: Colors.red,
+    backgroundColor: Colors.black,
+    ),
+    ):SizedBox(),
+    Expanded(
+    child: WebView(
+    initialUrl: url,
+    javascriptMode: JavascriptMode.unrestricted,
+    onProgress:(progress){
+    setState(() {
+    this.webprogress=progress/100;
+    });
+    },
+    ),
+    ),
+    ],
+    ),
+
 
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -55,6 +79,11 @@ class _Web_ViewState extends State<Web_View> {
         },
         child: Icon(Icons.import_export),
       ),
+    ):Container(
+      child: Center(
+        child: Text('No Internet ',style: TextStyle(color: Colors.white,fontSize: 24),),
+      ),
     );
+
   }
 }
