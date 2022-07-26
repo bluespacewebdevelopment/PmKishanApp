083d9344a1236724d179b20c328c13de6517f55e
BhagchandJat12 <6686@vitj.ac.in>
2022-07-26 18:24:41 +0530

    bhag1

---
+++ lib/utils/demo.dart
@@ -0,0 +1,39 @@
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+
+class Web_View extends StatefulWidget {
+  Web_View({Key? key, required this.url}) : super(key: key);
+  final String url;
+  @override
+  State<Web_View> createState() => _Web_ViewState(this.url);
+}
+
+class _Web_ViewState extends State<Web_View> {
+  String url;
+  late WebViewController controller;
+
+  _Web_ViewState(this.url);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('किसान सम्मान निधि'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0.2,
+      ),
+      body: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          controller.loadUrl('https://www.youtube.com/channel/UC7LI0mBeuP5zzrCf8t-vNlg');
+
+        },
+        child: Icon(Icons.import_export),
+      ),
+    );
+  }
+}
